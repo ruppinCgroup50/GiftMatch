39a5a3fcc44a69cadc344bc4b2e8262ec3d3d51f
Hodaya <hodishay@gmail.com>
2024-05-17 10:56:43 +0300

    המשך העלאת תמונה

--- GIftMatch server side/GiftMatchServer/GiftMatchServer/Controllers/UploadController.cs
+++ GIftMatch server side/GiftMatchServer/GiftMatchServer/Controllers/UploadController.cs
@@ -1,29 +1,17 @@
-﻿using AdHocObjects.Models;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting.Internal;
 using System.Net;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AdHocObjects.Controllers
+namespace GiftMatchServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UploadController : ControllerBase
     {
-        // GET: api/<UploadController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<UploadController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         // POST api/<UploadController>
         [HttpPost]
@@ -56,16 +44,6 @@ namespace AdHocObjects.Controllers
 
         }
 
-        // PUT api/<UploadController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UploadController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      
     }
 }
