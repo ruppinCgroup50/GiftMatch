4ee8d92bbbf6f63cef6cb0466a017d7406e01916
GiftMatch <158814513+MayManzur@users.noreply.github.com>
2024-08-12 23:48:54 +0300

    שינויים

--- GIftMatch server side/GiftMatchServer/GiftMatchServer/Controllers/GiftMatchController.cs
+++ GIftMatch server side/GiftMatchServer/GiftMatchServer/Controllers/GiftMatchController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace GiftMatchServer.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class GiftMatchController : ControllerBase
     {
         [HttpPost("SubmitIdea")]
